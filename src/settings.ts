c8e5b3583532a5dd794e4874b12a0f21d5715d5b
Andreas Wäscher <awaescher@gmx.de>
2024-06-30 14:00:59 +0200

    Update settings.ts

--- src/settings.ts
+++ src/settings.ts
@@ -1,9 +1,9 @@
 /**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
-export const PLATFORM_NAME = 'HttpJsonThermometer';
+export const PLATFORM_NAME = 'HttpJsonEvalThermometer';
 
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = 'homebridge-http-json-thermometer';
+export const PLUGIN_NAME = 'homebridge-http-json-eval-thermometer';
